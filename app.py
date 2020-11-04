7269733eda005e16655851753f285cf4b263cdf2
Scott Hao <scotthao65@gmail.com>
2020-11-03 22:06:35 -0500

    Created virtual env, renamed stuff

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    render_template('/views/index.html')
+
+if (__name__ == '__main__'):
+    app.run(debug=True, host='0.0.0.0')
