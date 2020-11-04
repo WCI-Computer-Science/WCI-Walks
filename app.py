c8ed51fdccd904d9c2263f77c94749e14d003a80
Scott Hao <scotthao65@gmail.com>
2020-11-03 22:42:38 -0500

    Fixed up main page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    render_template('/views/index.html')
+    return render_template('index.html', message='123')
 
 if (__name__ == '__main__'):
     app.run(debug=True, host='0.0.0.0')
