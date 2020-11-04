5e1d916e6e8923cc2457129257818caf0d6bf244
Scott Hao <scotthao65@gmail.com>
2020-11-04 09:59:55 -0500

    Added comments and auth router

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+
 from models import database
+from controllers import authentication
 
 app = Flask(__name__)
 
+# Create database and set up automatic database closing for requests
 app.config['DB'] = 'models/db.sqlite'
-
 database.init_app(app)
 
+# Route / to main page
 @app.route('/')
 def index():
-    return render_template('index.html', message='123')
+    return render_template('index.html')
 
+# Route /users to login and user statistics page
+app.register_blueprint(authentication.bp)
+
+# Test app for startup
 if (__name__ == '__main__'):
     app.run(debug=True, host='0.0.0.0')
--- controllers/authentication.py
+++ controllers/authentication.py
@@ -5,3 +5,6 @@ from database import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/users')
 
+@bp.route('/', methods=('GET', 'POST', 'PUT', 'PATCH', 'DELETE'))
+def login():
+    #etc
