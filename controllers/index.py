2df712f8a857d72aedc520d75daa31f1fabb9455
scott-22 <scotthao65@gmail.com>
2020-11-11 14:49:32 -0500

    Moved utils.py to templates

--- controllers/index.py
+++ controllers/index.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, session
-from .utils import get_leaderboard
+from templates.utils import get_leaderboard
 bp = Blueprint('index', __name__, url_prefix='/')
 
 @bp.route('/', methods=('GET', 'POST', 'PUT', 'PATCH', 'DELETE'))
