51fcaf157264ad872a81879404fdd9d0b38b994d
scott-22 <scotthao65@gmail.com>
2020-11-12 12:27:59 -0500

    used faster .items builtin, swapped bubble sort for timsort, check comments for more details

--- templates/utils.py
+++ templates/utils.py
@@ -1,28 +1,24 @@
 from models import database
+
+# note 1: hopefully in the future we could have an "online" ranking
+# already in sorted order and add users with a log2 n binary search
+
 def get_all_time_leaderboard():
     db = database.get_db()
-    userdistances = list(db.execute("SELECT username, distance FROM users;").fetchall())
-
-    for _ in range(len(userdistances) - 1):
-        for i in range(len(userdistances) - 1):
-            if userdistances[i][1] < userdistances[i + 1][1]:
-                userdistances[i], userdistances[i + 1] = userdistances[i + 1], userdistances[i]
-            elif userdistances[i][1] == userdistances[i + 1][1] and userdistances[i][0]>userdistances[i+1][0]:
-                userdistances[i], userdistances[i + 1] = userdistances[i + 1], userdistances[i]
-    for i in range(len(userdistances)):
-        userdistances[i]=list(userdistances[i])
+    # 3 things
+    # dict.items builtin 3 times faster than list(dict) function
+    # builtin .sort method uses Timsort for O(nlogn), while bubble sort is pretty bad even among other O(n^2) functions
+    # no need to convert to list, tuple is iterable
+    userdistances = db.execute(
+        "SELECT username, distance FROM users;"
+    ).fetchall().items().sorted(key=lambda user: user[1])
     return userdistances
 
 def get_day_leaderboard(date):
     db = database.get_db()
-    userdistances = list(db.execute("SELECT id, distance FROM walks WHERE walkdate=?;", (date,)).fetchall())
-
-    for _ in range(len(userdistances) - 1):
-        for i in range(len(userdistances) - 1):
-            if userdistances[i][1] < userdistances[i + 1][1]:
-                userdistances[i], userdistances[i + 1] = userdistances[i + 1], userdistances[i]
-            elif userdistances[i][1] == userdistances[i + 1][1] and userdistances[i][0]>userdistances[i+1][0]:
-                userdistances[i], userdistances[i + 1] = userdistances[i + 1], userdistances[i]
+    userdistances = db.execute(
+        "SELECT id, distance FROM walks WHERE walkdate=?;", (date,)
+    ).fetchall().items().sorted(key=lambda user: user[1]) 
     for i in range(len(userdistances)):
         userdistances[i]=list(userdistances[i])
         userdistances[i][0] = list(db.execute("SELECT username FROM users WHERE id=?;", (userdistances[i][0],)).fetchone())[0]
