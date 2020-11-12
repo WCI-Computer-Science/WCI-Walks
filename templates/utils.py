150c9e6f93b17e543b1fad3e9d497e197c40eb1b
scott-22 <scotthao65@gmail.com>
2020-11-12 13:06:21 -0500

    added constraint on number of students being ranked

--- templates/utils.py
+++ templates/utils.py
@@ -4,6 +4,12 @@ from models import database
 # note 1: hopefully in the future we could have an "online" ranking
 # already in sorted order and add users with a log2 n binary search
 
+# note 2: only shows top 15 for all time, top 10 for yesterday's
+# this does 2 things: makes sure first that if there's 100 students participating,
+# there's not an outrageous amount of users displayed
+# second, it reduces the max amount of items in the list so distances
+# being passed to front end isn't huge
+
 def get_all_time_leaderboard():
     db = database.get_db()
     # 3 things
@@ -14,7 +20,7 @@ def get_all_time_leaderboard():
     ).fetchall()
     userdistances.sort(key=lambda user: user[1], reverse=True)
     
-    return userdistances
+    return userdistances[:15]
 
 def get_day_leaderboard(date):
     db = database.get_db()
@@ -24,5 +30,5 @@ def get_day_leaderboard(date):
     ).fetchall()
     userdistances.sort(key=lambda user: user[1], reverse=True)
 
-    return userdistances
+    return userdistances[:10]
 
