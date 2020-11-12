ef5f22216a8b33a6d651cf9291cb2fb497a5e7fa
awenelo <56133692+awenelo@users.noreply.github.com>
2020-11-12 08:54:38 -0500

    Sort ties alphabetically on leaderboard

--- templates/utils.py
+++ templates/utils.py
@@ -7,6 +7,8 @@ def get_leaderboard():
         for i in range(len(userdistances) - 1):
             if userdistances[i][1] < userdistances[i + 1][1]:
                 userdistances[i], userdistances[i + 1] = userdistances[i + 1], userdistances[i]
+            elif userdistances[i][1] == userdistances[i + 1][1] and userdistances[i][0]>userdistances[i+1][0]:
+                userdistances[i], userdistances[i + 1] = userdistances[i + 1], userdistances[i]
     for i in range(len(userdistances)):
         userdistances[i]=list(userdistances[i])
     return userdistances
