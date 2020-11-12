f33a198637c56c39ac33e217dfd874ec9ed3d044
scott-22 <scotthao65@gmail.com>
2020-11-12 12:47:58 -0500

    fixed last commit, directly using Row objects

--- templates/utils.py
+++ templates/utils.py
@@ -1,3 +1,4 @@
+import sys
 from models import database
 
 # note 1: hopefully in the future we could have an "online" ranking
@@ -6,19 +7,21 @@ from models import database
 def get_all_time_leaderboard():
     db = database.get_db()
     # 3 things
-    # dict.items builtin 3 times faster than list(dict) function
+    # .fetchall already returns a list of Row objects (kind of like tuples)
     # builtin .sort method uses Timsort for O(nlogn), while bubble sort is pretty bad even among other O(n^2) functions
-    # no need to convert to list, tuple is iterable
     userdistances = db.execute(
         "SELECT username, distance FROM users;"
-    ).fetchall().items().sorted(key=lambda user: user[1])
+    ).fetchall()
+    userdistances.sort(key=lambda user: user[1], reverse=True)
+    print(userdistances, file=sys.stderr)
     return userdistances
 
 def get_day_leaderboard(date):
     db = database.get_db()
     userdistances = db.execute(
         "SELECT id, distance FROM walks WHERE walkdate=?;", (date,)
-    ).fetchall().items().sorted(key=lambda user: user[1]) 
+    ).fetchall()
+    userdistances.sort(key=lambda user: user[1], reverse=True) 
     for i in range(len(userdistances)):
         userdistances[i]=list(userdistances[i])
         userdistances[i][0] = list(db.execute("SELECT username FROM users WHERE id=?;", (userdistances[i][0],)).fetchone())[0]
