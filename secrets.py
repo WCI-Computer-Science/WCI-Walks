8b8369e279db41993fb92e81eaf6bf6248bb57a1
scott-22 <scotthao65@gmail.com>
2020-11-30 17:01:43 -0500

    remove secrets again (I've generated new keys already so these ones are no longer used)

--- secrets.py
+++
@@ -1,4 +0,0 @@
-secret_key = b'\x0c_\xa4-\xe5\xae\xdb\xd9b\xa1\x0e\x14B\xce`;\xe9`\xae\x10r\x03\xcd\xa2'
-security_password_salt = b'\x9b\tYJ>\x1d\xfcO1.\x8b\xe39\x80\x1a\xd7'
-google_client_id = '633095008920-k189gd22dun1tkvffj77mg9koc0t33k0.apps.googleusercontent.com'
-google_client_secret = 'bv_jaLjfcabRqmoZJdBs0B0l'
