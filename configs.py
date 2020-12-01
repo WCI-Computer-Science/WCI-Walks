3850be9859d69fa788fdf0f25614c0b32f5e3141
Scott Hao <68250609+scott-22@users.noreply.github.com>
2020-11-30 20:35:10 -0500

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,10 +1,7 @@
 import os
-
 import secrets
 
-ENV = 'development'
 SECRET_KEY = secrets.secret_key
-SECURITY_PASSWORD_SALT = secrets.security_password_salt
 DB = os.environ['DATABASE_URL']
 GOOGLE_CLIENT_ID = secrets.google_client_id
 GOOGLE_CLIENT_SECRET = secrets.google_client_secret
