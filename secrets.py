1d38cc01f518cbe8edc94f6be34abdc416dd0909
scott-22 <scotthao65@gmail.com>
2020-11-30 20:26:49 -0500

    change

--- secrets.py
+++ secrets.py
@@ -1,4 +1,3 @@
 secret_key = b'e\xb8\x0c\xd8 \xec\xe2\x1ei\xb0"\xe6\xb1\xa8\x9f\xa6\x13\xd6%\x1fp^H\xea'
-security_password_salt = b"n^\n\xbef\xcf\x1e'XAI\x9d\x88\x9ee\xb1"
 google_client_id = '633095008920-k189gd22dun1tkvffj77mg9koc0t33k0.apps.googleusercontent.com'
 google_client_secret = 'TnxAfAD6BW-cG2Y9-P63iyKo'
