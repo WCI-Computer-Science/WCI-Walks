ddb8aaab6a42ad82f4c93a7fd433a6c19a6a15ac
awenelo <56133692+awenelo@users.noreply.github.com>
2020-12-17 09:04:08 -0500

    Add blank admin page

--- application/controllers/admin.py
+++ application/controllers/admin.py
@@ -1,3 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
+@bp.route("/")
+def adminhome():
+  return rendertemplate("adminpage.html")
