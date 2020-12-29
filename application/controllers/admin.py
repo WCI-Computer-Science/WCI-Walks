0d8bf9eb2d53d26bd07d0d880b35c2e225a78e36
awenelo <56133692+awenelo@users.noreply.github.com>
2020-12-29 12:30:27 -0500

    Add user search and description for admin buttons

--- application/controllers/admin.py
+++ application/controllers/admin.py
@@ -1,5 +1,5 @@
-from flask import abort, Blueprint, render_template, redirect
-from application.templates.utils import isadmin, update_total
+from flask import abort, Blueprint, render_template, redirect, request
+from application.templates.utils import isadmin, update_total, get_all_time_leaderboard
 from flask_login import current_user, login_required
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
@@ -19,3 +19,18 @@ def updatetotal():
     return render_template("updatetotalsuccess.html")
   else: abort(403)
 
+@bp.route("/getuserlist/")
+def getuserlist():
+  search = request.args.get("text", "")
+  userlist = get_all_time_leaderboard()
+  returnlist = list(userlist) # Using list() to seperate from userlist, otherwise returnlist would be tied to userlist
+  if search != "":
+    for i in userlist:
+      if search not in i[0]:
+        returnlist.remove(i)
+    returnlist.sort(key=lambda user:user[0])
+  return render_template("userlist.html", userlist=returnlist)
+
+@bp.route("/searchforuser/")
+def editdistances():
+  return render_template("searchforuser.html")
