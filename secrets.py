9e3f1dd4d47edba9fbbe7ab57dc98ba7af8b7374
scott-22 <scotthao65@gmail.com>
2021-01-04 13:40:56 -0500

    removed secrets

--- secrets.py
+++
@@ -1,3 +0,0 @@
-secret_key = b'o\x85\x8e\xc2\xba\xf2\x01\xee\x99\xe4|\xc0\x06\xd4\xc7x\x0b\x07\x9c\x87)p\xa1ZC\x19\x85[\xf1h\x81\x07'
-google_client_id = '633095008920-k189gd22dun1tkvffj77mg9koc0t33k0.apps.googleusercontent.com'
-google_client_secret = 'Vz_aUIvZvxQCcJh7iqMyWnbu'
