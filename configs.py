9bc9b64aa86d5653dc948a8d045bf311b4338e4e
scott-22 <scotthao65@gmail.com>
2021-01-04 13:51:57 -0500

    made secrets environment variables

--- configs.py
+++ configs.py
@@ -1,8 +1,14 @@
 import os
-import secrets
 
-SECRET_KEY = secrets.secret_key
+try:
+    import secrets
+    SECRET_KEY = secrets.secret_key
+    GOOGLE_CLIENT_ID = secrets.google_client_id
+    GOOGLE_CLIENT_SECRET = secrets.google_client_secret
+except:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+    GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
+
 DB = os.environ['DATABASE_URL']
-GOOGLE_CLIENT_ID = secrets.google_client_id
-GOOGLE_CLIENT_SECRET = secrets.google_client_secret
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
