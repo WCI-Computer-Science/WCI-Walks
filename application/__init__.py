744fe0f7309ebb0ad5c696c9a6570a11e2633b16
awenelo <56133692+awenelo@users.noreply.github.com>
2021-01-05 08:58:53 -0500

    Hopefully travis works now...

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import secrets, configs
+import configs
 
 def create_app():
 
--- configs.py
+++ configs.py
@@ -6,9 +6,14 @@ try:
     GOOGLE_CLIENT_ID = secrets.google_client_id
     GOOGLE_CLIENT_SECRET = secrets.google_client_secret
 except:
-    SECRET_KEY = os.environ['SECRET_KEY']
-    GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
-    GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
-
+    try:
+        SECRET_KEY = os.environ['SECRET_KEY']
+        GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+        GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
+    except: # Probably running on travis, no need for real values here
+        print("Warn: Could not load secrets, using bogus values.")
+        SECRET_KEY = ""
+        GOOGLE_CLIENT_ID = ""
+        GOOGLE_CLIENT_SECRET = ""
 DB = os.environ['DATABASE_URL']
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
