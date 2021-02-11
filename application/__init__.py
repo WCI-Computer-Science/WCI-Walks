c0ad913b3d1fdf2fc2552bad383c65c56229f951
awenelo <56133692+awenelo@users.noreply.github.com>
2021-02-11 15:05:55 -0500

    Fix travis?

--- application/__init__.py
+++ application/__init__.py
@@ -20,7 +20,7 @@ def create_app():
         from .models import database, loginmanager, oauth, user
         from .controllers import index, users, admin
         from .controllers.errors import error404, error500
-        from .templates.utils import update_tick, long_update_tick, update_leaderboard_positions
+        from .templates.utils import update_tick, long_update_tick
         scheduler = BackgroundScheduler()
         scheduler.add_job(update_tick, 'cron', minutes="*/5") # Thing that need to be polled
         scheduler.add_job(long_update_tick, 'cron', hour="1") # Long function that could interupt normal functioning, like refreshing totals. Runs at 1am
