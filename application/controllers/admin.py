59b6abf2a39b1fda74dc68cc43a2d9c3c90b9a0a
awenelo <56133692+awenelo@users.noreply.github.com>
2021-02-15 18:12:39 -0500

    Remove old editdistances stuff - resolves #20

--- application/controllers/admin.py
+++ application/controllers/admin.py
@@ -54,52 +54,6 @@ def getuserlist():
 def searchforuser():
     return render_template("searchforuser.html")
 
-
-@bp.route("/edituserdistances/old/<wrdsbusername>", methods=("GET", "POST"))
-@login_required
-def editdistancespage(wrdsbusername):
-    if not current_user.is_admin():
-        abort(403)
-    if request.method == "POST":
-        datetimedates = list()
-        distances = list()
-        dates = list()
-        olddistances = ast.literal_eval(request.form.get("alldistances"))
-        alldates = ast.literal_eval(request.form.get("alldates"))
-        for i in alldates:
-            distances.append(fancy_float(request.form.get(str(i))))
-            datetimedates.append(i)
-            dates.append(
-                datetime.strptime(i, "%Y-%m-%d").strftime("%A, %B %d, %Y")
-            )
-        replace_walk_distances(
-            distances,
-            datetimedates,
-            olddistances,
-            current_user,
-            id=get_credentials_from_wrdsbusername(wrdsbusername)[0],
-        )
-        if olddistances != distances:
-            update_total()
-    else:
-        db = database.get_db()
-        with db.cursor() as cur:
-            datetimedates, distances = current_user.get_walk_chart_data(
-                cur, id=get_credentials_from_wrdsbusername(wrdsbusername)[0]
-            )
-        dates = list()
-        distances = list(map(fancy_float, distances))
-        for i in datetimedates:
-            dates.append(i.strftime("%A, %B %d, %Y"))
-    datetimedates = list(map(str, datetimedates))
-    return render_template(
-        "oldeditdistances.html",
-        distances=distances,
-        dates=dates,
-        datetimedates=datetimedates,
-        user=get_credentials_from_wrdsbusername(wrdsbusername)[1],
-    )
-
 @bp.route("/edituserdistances/<wrdsbusername>", methods=("GET", "POST"))
 @login_required
 def newedituserdistancespage(wrdsbusername):
